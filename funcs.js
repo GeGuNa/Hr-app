84917e798a18f251e91f83923e236c25be503352
XphantomX <geguna27@gmail.com>
2022-11-25 00:22:20 +0400

    whocares :D

---
+++ funcs.js
@@ -0,0 +1,30 @@
+function Unix_timestamp(){
+
+const dt = new Date;
+
+return parseInt(dt.getTime()/1000);
+}
+
+
+function Is_number(id) {
+
+var cidck =  /^[0-9]+$/	
+
+//id = Math.abs(id)	
+	
+return cidck.test(id)
+}
+
+
+const knex = require('knex')({
+'client':'mysql',
+connection: {
+user:'',
+database:'',
+password: '',
+port:'',
+}
+});
+
+
+module.exports = { Unix_timestamp, Is_number, knex }
--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 
+var session = require('express-session')
+
+app.set('trust proxy', 1) 
+
 app.use(express.json())
 app.use(express.urlencoded({extended: true})); 
+
+
+app.use(session({
+  secret: 'whocares',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
 
 
 app.set('views', `${__dirname}/views/`)
@@ -16,6 +29,19 @@ app.use('/pics', express.static('pictures'))
 
 app.get("/", (req, res) => {
 
+//req.session.error = ''
+
+
+/*
+if (req.session.error !== undefined) {
+console.log(`--------------`)
+console.log(`test`)
+console.log(req.session.error)
+console.log(`--------------`)
+}
+*/
+
+
 res.render(`index.ejs`)
 
 res.end()
@@ -23,11 +49,36 @@ res.end()
 
 app.get("/in", (req, res) => {
 
-res.render(`login.ejs`)
+res.render(`login.ejs`, {error: ''})
 
 res.end()
 })
 
+
+app.post("/in", (req, res) => {
+
+let qz = req.body.mail
+let qz2 = req.body.pass
+
+
+console.log(qz)
+console.log(qz2)
+
+/*
+
+req.session.error = '';
+let qz = req.session.error
+console.log(qz)
+
+*/
+
+
+
+res.write(`qweqweqwe`)
+
+
+res.end()
+})
 
 
 app.get("*", (req, res) => {
