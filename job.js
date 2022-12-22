52160fdf3a80c8df5d87527110a7be95c6c2dd64
XphantomX <geguna27@gmail.com>
2022-12-23 03:47:51 +0400

    Job details

--- job.js
+++ job.js
@@ -34,8 +34,6 @@ return res.redirect('/')
 
 
 let qz221a = await knex('job').where("jid",rquid).select('*').first()
-let cmpnid = await knex('company').where("cid",qz221a.cid).select('*').first()
-let jpostuser = await knex('user').where("uid",qz221a.user).select('*').first()
 
 
 if (!qz221a) {
@@ -44,7 +42,35 @@ return res.redirect('/')
 	
 }
 
-//console.log(jpostuser)
+
+
+let cmpnid = await knex('company').where("cid",qz221a.cid).select('*').first()
+let jpostuser = await knex('user').where("uid",qz221a.user).select('*').first()
+
+
+
+
+
+
+/*
+RowDataPacket {
+  jid: 1,
+  user: 1,
+  cid: 22,
+  time: null,
+  expire_time: null,
+  name: 'From  zzVidea',
+  mail: 'asds@gmail.com',
+  number: '995555445522',
+  subject: 'buhahahah',
+  desc: 'qweqweqweqweqweqweqweqweqweqweqweqwe',
+  work_type: '',
+  empl_type: '',
+  experience_time: ''
+}
+console.log(qz221a)
+*/
+
 
 
 let psrq = {title:'Job details', user: tdatausr, jdata: qz221a, cdata: cmpnid, usrdata: jpostuser}
